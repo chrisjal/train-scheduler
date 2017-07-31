3ce3de7423f820e57cbb370d8e9a631fe7759e50
chrisjal <cjalallian@gmail.com>
2017-07-31 14:25:57 -0700

    basic bootstrap styling and firebase init

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDLqIU7nSPsZ7Z2HP77jHeiWPMBbd0FUgc",
+	authDomain: "train-scheduler-9189d.firebaseapp.com",
+	databaseURL: "https://train-scheduler-9189d.firebaseio.com",
+	projectId: "train-scheduler-9189d",
+	storageBucket: "train-scheduler-9189d.appspot.com",
+	messagingSenderId: "993913339106"
+};
+firebase.initializeApp(config);
+
