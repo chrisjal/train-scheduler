bd43bdd7728299e1f4c98bf4d2aba3fc0e3e1ac3
chrisjal <cjalallian@gmail.com>
2017-08-01 12:56:49 -0700

    added working append from DB/logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,3 +9,53 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
+
+var trainName = "";
+var trainDestination = "";
+var trainFirst = "";
+var trainFrequency = "";
+
+$("#add-train").on("click", function() {
+	event.preventDefault();
+
+	var trainName = $("#input-name").val().trim();
+	var trainDestination = $("#input-destination").val().trim();
+	var trainFirst = moment($("#input-first-train").val().trim(), "HH:mm").subtract(10, "years").format("X");
+	var trainFrequency = $("#input-frequency").val().trim();
+
+	database.ref().push({
+		name: trainName,
+		destination: trainDestination,
+		firstTime: trainFirst,
+		frequency: trainFrequency
+	});
+	// Clear form
+	$('form').trigger("reset");
+});
+
+// database.ref().on("value", function(snapshot) {
+// 	console.log(snapshot.val());
+// 	var tableName = snapshot.val().name;
+// 	var tableDestination = snapshot.val().destination;
+// 	var tableFirstTime = snapshot.val().firstTime;
+// 	var tableFrequency = snapshot.val().frequency;
+
+// 	$("#train-table").append("<tr><td>" + tableName + "</td><td>" + tableDestination + "</td><td>" + tableFirstTime + "</td><td>" + tableFrequency + "</td></tr>");
+
+// 	}, function(errorObject) {
+// 		console.log("Errors handled: " + errorObject.code);
+// });
+
+database.ref().on("child_added", function(childSnapshot){
+	console.log(childSnapshot.val());
+	var tableName = childSnapshot.val().name;
+	var tableDestination = childSnapshot.val().destination;
+	var tableFirstTime = childSnapshot.val().firstTime;
+	var tableFrequency = childSnapshot.val().frequency;
+
+	$("#train-table").append("<tr><td>" + tableName + "</td><td>" + tableDestination + "</td><td>" + tableFirstTime + "</td><td>" + tableFrequency + "</td></tr>");
+
+	}, function(errorObject) {
+		console.log("Errors handled: " + errorObject.code);
+});
