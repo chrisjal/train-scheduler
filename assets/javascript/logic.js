aa39ede29a3f66e8ddf61b62c917cc040ed83c1d
chrisjal <cjalallian@gmail.com>
2017-08-01 13:28:34 -0700

    fixed table, added needed variables for future time adjustments

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,20 +15,25 @@ var trainName = "";
 var trainDestination = "";
 var trainFirst = "";
 var trainFrequency = "";
+var next = "";
+var minutesAway = "";
 
 $("#add-train").on("click", function() {
 	event.preventDefault();
 
 	var trainName = $("#input-name").val().trim();
 	var trainDestination = $("#input-destination").val().trim();
-	var trainFirst = moment($("#input-first-train").val().trim(), "HH:mm").subtract(10, "years").format("X");
+	// var trainFirst = moment($("#input-first-train").val().trim(), "HH:mm").subtract(10, "years").format("X");
+	var trainFirst = $("#input-first-train").val().trim();
 	var trainFrequency = $("#input-frequency").val().trim();
 
 	database.ref().push({
 		name: trainName,
 		destination: trainDestination,
 		firstTime: trainFirst,
-		frequency: trainFrequency
+		frequency: trainFrequency,
+		next: nextTrain,
+		minAway: minutesAway
 	});
 	// Clear form
 	$('form').trigger("reset");
@@ -53,8 +58,10 @@ database.ref().on("child_added", function(childSnapshot){
 	var tableDestination = childSnapshot.val().destination;
 	var tableFirstTime = childSnapshot.val().firstTime;
 	var tableFrequency = childSnapshot.val().frequency;
+	var tableNext = childSnapshot.val().next;
+	var tableAway = childSnapshot.val().minAway;
 
-	$("#train-table").append("<tr><td>" + tableName + "</td><td>" + tableDestination + "</td><td>" + tableFirstTime + "</td><td>" + tableFrequency + "</td></tr>");
+	$("#train-table").append("<tr><td>" + tableName + "</td><td>" + tableDestination + "</td><td>" + tableFrequency + "</td><td>" + tableNext + "</td><td>" + tableAway + "</td></tr>");
 
 	}, function(errorObject) {
 		console.log("Errors handled: " + errorObject.code);
